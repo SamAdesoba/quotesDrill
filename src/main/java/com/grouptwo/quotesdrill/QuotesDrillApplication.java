fc9d01075c60c89242d8a64857c38e928c78dac9
Adefolarin Samson Adesoba <a.adesoba@native.semicolon.africa>
2022-10-11 11:33:35 +0100

    Initial commit

---
+++ src/main/java/com/grouptwo/quotesdrill/QuotesDrillApplication.java
@@ -0,0 +1,13 @@
+package com.grouptwo.quotesdrill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuotesDrillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuotesDrillApplication.class, args);
+	}
+
+}

a6b1ca7c78494654ef89726eac25a60131afbd17
Swethead <97471699+Ortopaz@users.noreply.github.com>
2022-10-11 17:49:57 +0100

    push

---
+++ src/main/java/com/grouptwo/quotesdrill/controller/ApiController.java
@@ -0,0 +1,12 @@
+package com.grouptwo.quotesdrill.controller;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@RequestMapping("/api/Api")
+public class ApiController {
+}
